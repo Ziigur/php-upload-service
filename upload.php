54d7c67f01abcf12c9df9732f47d5e5ad1e4208e
Ziigur <zigur94@gmail.com>
2025-03-31 10:41:38 +0200

    Make responses work better with CLI tools

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
-    echo "Use POST to upload files";
+    echo "Use POST to upload files\n";
     exit;
 }
 if (!isset($_FILES['file'])) {
     http_response_code(400);
-    echo "No file uploaded";
+    echo "No file uploaded\n";
     exit;
 }
