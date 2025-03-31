16739ec4750c9b22f065144eb028aedd15795002
Ziigur <zigur94@gmail.com>
2025-03-31 12:36:25 +0200

    Add API description to index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-  echo "Hello World!\n";
+header('Content-Type: application/json');
+$host = $_SERVER['HTTP_HOST']; // Get the server host
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http"; // Handle HTTP/HTTPS
+$fullUrl = $protocol . "://" . $host;
+
+$output = [];
+$output['actions'] = array();
+array_push(
+    $output['actions'],
+    array("action"=>"upload", "url"=>$fullUrl . '/upload.php', "method"=>"POST")
+);
+
+echo json_encode($output, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+echo "\n";
